c9730b9cd2ff6c6028daf819bcd2309d6138f55a
Juls <talaevaiulia@yandex.ru>
2024-10-29 10:36:32 +0300

    Delete init and safe app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,18 @@
 from flask import Flask
-from . import app, users
-from .views import index
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+import config
 
-from .models import News, Users
+app = Flask(__name__, template_folder='templates')
+app.config.from_object(config.Config)
+
+# Инициализация SQLAlchemy и Bcrypt
+users = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+# Создание всех таблиц в базе данных
 with app.app_context():
     users.create_all()
+
 if __name__ == "__main__":
     app.run(debug=True)
-
